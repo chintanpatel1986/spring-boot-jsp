b5fd539e13fd86b1edab1cf75f493002837ca10f
chintanpatel <chintanpatel2013@icloud.com>
2024-11-08 10:36:05 -0500

    Create EmployeeController.java file for this application.

---
+++ src/main/java/org/chintanpatel/springbootjsp/controller/EmployeeController.java
@@ -0,0 +1,72 @@
+package org.chintanpatel.springbootjsp.controller;
+
+import jakarta.validation.Valid;
+import org.chintanpatel.springbootjsp.model.Employee;
+import org.chintanpatel.springbootjsp.service.DepartmentService;
+import org.chintanpatel.springbootjsp.service.EmployeeService;
+import org.chintanpatel.springbootjsp.service.RoleService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+    private final DepartmentService departmentService;
+    private final RoleService roleService;
+
+    public EmployeeController(EmployeeService employeeService, DepartmentService departmentService, RoleService roleService) {
+        this.employeeService = employeeService;
+        this.departmentService = departmentService;
+        this.roleService = roleService;
+    }
+
+
+    @GetMapping("/employees/getEmployee")
+    public String getEmployee(Model model) {
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("departmentList", departmentService.getAllDepartmentList());
+        model.addAttribute("roleList", roleService.getAllRoleList());
+        model.addAttribute("employeeList", employeeService.getAllEmployeeList());
+        return "employee-form";
+    }
+
+
+    @PostMapping("/employees/saveEmployee")
+    public String saveEmployee(@Valid @ModelAttribute("employee")Employee employee, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("departmentList", departmentService.getAllDepartmentList());
+            model.addAttribute("roleList", roleService.getAllRoleList());
+            model.addAttribute("employeeList", employeeService.getAllEmployeeList());
+            return "employee-form";
+        }
+        employeeService.addEmployee(employee);
+        return "redirect:/employees/getEmployee";
+    }
+
+
+    @GetMapping("/employees/manageEmployee")
+    public String manageEmployee(@RequestParam("employeeId")Long employeeId, Model model) {
+        if (employeeId != null) {
+            model.addAttribute("employee", employeeService.getEmployeeById(employeeId));
+            model.addAttribute("departmentList", departmentService.getAllDepartmentList());
+            model.addAttribute("roleList", roleService.getAllRoleList());
+            model.addAttribute("employeeList", employeeService.getAllEmployeeList());
+        }
+        return "employee-form";
+    }
+
+
+    @GetMapping("/employees/deleteEmployee")
+    public String deleteEmployee(@RequestParam("employeeId")Long employeeId) {
+        if (employeeId != null) {
+            employeeService.deleteEmployeeById(employeeId);
+        }
+        return "redirect:/employees/getEmployee";
+    }
+}
