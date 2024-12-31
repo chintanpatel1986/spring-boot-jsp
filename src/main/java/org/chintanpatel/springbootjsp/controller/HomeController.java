e6fea7459253c98d24966ee31389705dfb684bff
chintanpatel1986 <chintanpatel2013@icloud.com>
2024-12-31 11:17:53 -0500

    Update application.properties file and create HomeController and index.jsp file for project.

---
+++ src/main/java/org/chintanpatel/springbootjsp/controller/HomeController.java
@@ -0,0 +1,19 @@
+package org.chintanpatel.springbootjsp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/back")
+    public String back() {
+        return "index";
+    }
+}
